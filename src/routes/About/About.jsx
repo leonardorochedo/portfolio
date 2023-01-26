e6a247ec77f32816262f151f3c3eb1e8e865455a
Leonardo Ferreira Rochedo <62243365+leonardorochedo@users.noreply.github.com>
2023-01-26 18:58:40 -0300

    Update myAge

--- src/routes/About/About.jsx
+++ src/routes/About/About.jsx
@@ -12,9 +12,13 @@ import perfilImg from '../../assets/about/perfil.jpg'
 
 export function About() {
 
-    const atualYear = new Date().getFullYear()
-
-    const myAge = parseInt(atualYear) - 2002
+    const atualYear = new Date();
+    const myDate = '2002-07-16';
+    const diffInMs   = new Date(atualYear) - new Date(myDate)
+    const diffInDays = diffInMs / (1000 * 60 * 60 * 24);
+    const diffInYears = diffInDays/364;
+    
+    const myYear = diffInYears.split(".")[0];
 
     return (
         <>
@@ -24,7 +28,7 @@ export function About() {
                 <img className='perfil' src={perfilImg} alt="Foto de perfil" />
             </div>
             <div className="textAbout">
-                <p>Me chamo Leonardo atualmente tenho {myAge} anos e estou cursando TADS no IFPR de Londrina, sempre fui apaixonado por tecnologia desde pequeno e esse lado meu só cresce!
+                <p>Me chamo Leonardo atualmente tenho {myYear} anos e estou cursando TADS no IFPR de Londrina, sempre fui apaixonado por tecnologia desde pequeno e esse lado meu só cresce!
                 </p>
                 <p>
                 Estou focando meus estudos atualmente voltado para o <i>Desenvolvimento Full Stack</i> e estou gostando muito, porém não me prendo apenas nisso e quero a cada dia aprender novas coisas.
