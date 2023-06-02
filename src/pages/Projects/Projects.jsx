aee356e19bfff955fbcad6ebda39e5890e871d5a
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-06-02 13:33:40 +0000

    add : object data mapping

--- src/pages/Projects/Projects.jsx
+++ src/pages/Projects/Projects.jsx
@@ -84,14 +84,11 @@ export function Projects() {
                     slidesPerView={1}
                     pagination={{ clickable: true, dynamicBullets: true }}
                     >
-                        {Object.keys(fullstack).map((app) => {
+                        {Object.keys(fullstack).map((app, index) => {
                             const project = fullstack[app];
 
-                            return <SwiperSlide><ProjectCard title={project.title} text={project.text} link={project.link} image={backNewCars} icon1={verifyImage(project.icon1)} icon2={project.icon2} icon3={project.icon3}/></SwiperSlide>
+                            return <SwiperSlide key={index} ><ProjectCard title={project.title} text={project.text} link={project.link} image={backNewCars} icon1={project.icon1} icon2={project.icon2} icon3={project.icon3}/></SwiperSlide>
                         })}
-                        {/* <SwiperSlide><ProjectCard title={fullstack.dsmeta.title} text={fullstack.dsmeta.text} link={fullstack.dsmeta.link} image={backDsmeta} icon1={reactImg} icon2={springImg} icon3={mysqlImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={fullstack.getapet.title} text={fullstack.getapet.text} link={fullstack.getapet.link} image={backGetAPet} icon1={reactImg} icon2={nodeImg} icon3={mongoImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={fullstack.toughts.title} text={fullstack.toughts.text} link={fullstack.toughts.link} image={backToughts} icon1={nodeImg} icon2={hbsImg} icon3={mysqlImg}/></SwiperSlide> */}
                     </Swiper>
                 </div>
             </div>
@@ -104,13 +101,11 @@ export function Projects() {
                     slidesPerView={1}
                     pagination={{ clickable: true, dynamicBullets: true }}
                     >
-                        <SwiperSlide><ProjectCard title={backend.msspring.title} text={backend.msspring.text} link={backend.msspring.link} image={backMsspring} icon1={springImg} icon2={mysqlImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={backend.workspringmongo.title} text={backend.workspringmongo.text} link={backend.workspringmongo.link} image={backWorkspringmongo} icon1={springImg} icon2={mongoImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={backend.workspring.title} text={backend.workspring.text} link={backend.workspring.link} image={backWorkspring} icon1={springImg} icon2={mysqlImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={backend.daojdbc.title} text={backend.daojdbc.text} link={backend.daojdbc.link} image={backDaojdbc} icon1={javaImg} icon2={mysqlImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={backend.chessmatch.title} text={backend.chessmatch.text} link={backend.chessmatch.link} image={backChessmatch} icon1={javaImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={backend.nodejsmongo.title} text={backend.nodejsmongo.text} link={backend.nodejsmongo.link} image={backNodejsmongo} icon1={nodeImg} icon2={mongoImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={backend.bankaccount.title} text={backend.bankaccount.text} link={backend.bankaccount.link} image={backBankaccount} icon1={nodeImg}/></SwiperSlide>
+                        {Object.keys(backend).map((app, index) => {
+                            const project = backend[app];
+
+                            return <SwiperSlide key={index} ><ProjectCard title={project.title} text={project.text} link={project.link} image={backNewCars} icon1={project.icon1} icon2={project.icon2} icon3={project.icon3}/></SwiperSlide>
+                        })}
                     </Swiper>
                 </div>
             </div>
@@ -123,14 +118,11 @@ export function Projects() {
                     slidesPerView={1}
                     pagination={{ clickable: true, dynamicBullets: true }}
                     >
-                        <SwiperSlide><ProjectCard title={frontend.paduan.title} text={frontend.paduan.text} link={frontend.paduan.link} image={backPaduan} icon1={reactImg} icon2={cssImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={frontend.sunrain.title} text={frontend.sunrain.text} link={frontend.sunrain.link} image={backSunrain} icon1={reactImg} icon2={tsImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={frontend.pokedex.title} text={frontend.pokedex.text} link={frontend.pokedex.link} image={backPokedex} icon1={reactImg} icon2={tsImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={frontend.rocketcard.title} text={frontend.rocketcard.text} link={frontend.rocketcard.link} image={backRocketcard} icon1={reactImg} icon2={jsImg} icon3={tsImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={frontend.listapresenca.title} text={frontend.listapresenca.text} link={frontend.listapresenca.link} image={backListPresen} icon1={reactImg} icon2={jsImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={frontend.countdown.title} text={frontend.countdown.text} link={frontend.countdown.link} image={backCountdown} icon1={reactImg} icon2={jsImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={frontend.fotonovo.title} text={frontend.fotonovo.text} link={frontend.fotonovo.link} image={backFotonovo} icon1={htmlImg} icon2={cssImg} icon3={jsImg}/></SwiperSlide>
-                        <SwiperSlide><ProjectCard title={frontend.jta.title} text={frontend.jta.text} link={frontend.jta.link} image={backJta} icon1={htmlImg} icon2={cssImg} icon3={jsImg}/></SwiperSlide>
+                        {Object.keys(frontend).map((app, index) => {
+                            const project = frontend[app];
+
+                            return <SwiperSlide key={index}><ProjectCard title={project.title} text={project.text} link={project.link} image={backNewCars} icon1={project.icon1} icon2={project.icon2} icon3={project.icon3}/></SwiperSlide>
+                        })}
                     </Swiper>
                 </div>
             </div>
