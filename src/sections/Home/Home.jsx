28addc510a610494e010b034ebcbf2cb4630c46c
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-11-23 13:45:33 +0000

    fix: buttonHome

--- src/sections/Home/Home.jsx
+++ src/sections/Home/Home.jsx
@@ -4,6 +4,14 @@ import groovyWalkAnimation from "./groovyWalkAnimation.json";
 import './Home.css';
 
 export function Home() {
+
+    const scrollToContact = () => {
+        const contactDiv = document.getElementById('contact');
+        if (contactDiv) {
+            contactDiv.scrollIntoView({ behavior: 'smooth' });
+        }
+    };
+
     return (
         <div className="container-home" id="home">
             <div className="walk">
@@ -11,11 +19,11 @@ export function Home() {
             </div>
             <div className="text">
                 <div className="title">
-                    <h1>Olá! Me chamo <span className="name">Leonardo</span><span className="hand">👋</span></h1>
+                    <h1>Olá! Me chamo <span className="name">Leonardo Rochedo</span><span className="hand">👋</span></h1>
                     <p>Desenvolvedor Full Stack</p>
                 </div>
                 <div className="buttons">
-                    <button>
+                    <button onClick={scrollToContact}>
                         Entrar em contato
                     </button>
                 </div>
