483b293338ad86b5cf2c734015e3369c367f7451
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-11-28 00:10:23 +0000

    fix: header height

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 // Imagens
 import arrowClose from '../../assets/header/close-nav.svg'
@@ -8,56 +8,62 @@ import './Header.css'
 
 export function Header() {
 
-    let [count, setCount] = useState(1)
+    const [isMenuOpen, setMenuOpen] = useState(null);
+    const isMobile = window.innerWidth <= 1000;
 
-    let navImage;
+    const toggleMenu = () => {
+        setMenuOpen(!isMenuOpen);
+    };
 
-    // Alterar o nav icon
-    if (count % 2 == 0) {
-        navImage = arrowClose
-    } else {
-        navImage = arrowOpen
-    }
+    const closeMenu = () => {
+        setMenuOpen(false);
+    };
 
-    function openModal() {
-        setCount(count = count + 1)
-
-        if (count % 2 == 0) {
-            document.querySelector('.control-header').style.display = 'flex';
-            document.querySelector('.header-projects').style.display = 'flex';
-        } else {
-            document.querySelector('.control-header').style.display = 'none';
-            document.querySelector('.header-projects').style.display = 'none';
+    useEffect(() => {
+        if (isMobile) {
+            if (isMenuOpen) {
+                document.querySelector('.container').style.display = 'none';
+                document.querySelector('.control-header').style.display = 'flex';
+                document.querySelector('.header-projects').style.display = 'flex';
+            } else if (isMenuOpen == false) {
+                document.querySelector('.container').style.display = 'flex';
+                document.querySelector('.control-header').style.display = 'none';
+                document.querySelector('.header-projects').style.display = 'none';
+            }
         }
-    }
+    }, [isMenuOpen])
+
+    const navImage = isMenuOpen ? arrowClose : arrowOpen;
 
     return (
         <header>
-        <div className="control-button">
-            <button id='openButton' onClick={openModal}><img id='navicon' src={navImage} alt="Menu" /></button>
-        </div>
-        <div className="control-header">
-            <div className='header-projects'>
-                <ul>
-                    <li className='home'>
-                        <span className='menu'></span>
-                        <a className='menu' href='#home'>Início</a>
-                    </li>
-                    <li className='tec'>
-                        <span className='menu'></span>
-                        <a className='menu' href='#techs'>Tecnologias</a>
-                    </li>
-                    <li className='proj'>
-                        <span className='menu'></span>
-                        <a className='menu' href='#projects'>Projetos</a>
-                    </li>
-                    <li className='proj'>
-                        <span className='menu'></span>
-                        <a className='menu' href='#contact'>Contato</a>
-                    </li>
-                </ul>
+            <div className="control-button">
+                <button id='openButton' onClick={toggleMenu}>
+                    <img id='navicon' src={navImage} alt="Menu"/>
+                </button>
             </div>
-        </div>
+            <div className="control-header">
+                <div className='header-projects'>
+                    <ul>
+                        <li className='home'>
+                            <span className='menu'></span>
+                            <a className='menu' href='#home' onClick={closeMenu}>Início</a>
+                        </li>
+                        <li className='tec'>
+                            <span className='menu'></span>
+                            <a className='menu' href='#techs' onClick={closeMenu}>Tecnologias</a>
+                        </li>
+                        <li className='proj'>
+                            <span className='menu'></span>
+                            <a className='menu' href='#projects' onClick={closeMenu}>Projetos</a>
+                        </li>
+                        <li className='proj'>
+                            <span className='menu'></span>
+                            <a className='menu' href='#contact' onClick={closeMenu}>Contato</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
         </header>
     );
 }
