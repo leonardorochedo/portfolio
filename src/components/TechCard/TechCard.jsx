1b715f0530df6707e43381cdd618c010194590b6
Leonardo Ferreira Rochedo <learnir@outlook.com.br>
2023-11-28 00:20:00 +0000

    fix: projects size

--- src/components/TechCard/TechCard.jsx
+++ src/components/TechCard/TechCard.jsx
@@ -4,7 +4,7 @@ import "./TechCard.css";
 
 export function TechCard({ children, name }) {
     return (
-        <Tilt glareEnable='true' glareColor='#6161f7' glareMaxOpacity='1' className='card'>
+        <Tilt glareEnable='true' glareColor='#6161f7' glareMaxOpacity='1' glareBorderRadius='20px' className='card'>
             <p className='title'>{name}</p>
             {children}
         </Tilt>
