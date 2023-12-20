aeaf7232062e8c4755002199dbea31e3dbf6982c
Leonardo Ferreira Rochedo <62243365+leonardorochedo@users.noreply.github.com>
2023-12-20 16:37:50 -0300

    Update About.jsx

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -5,7 +5,7 @@ export function About() {
         <div className="container-about" id="about">
             <h1 className="title">Sobre mim</h1>
             <p>Desenvolvedor Full Stack especializado em Backend com experiência em Java, Spring Boot, JavaScript, TypeScript, SQL, NoSQL, Arquitetura Microsserviços e também Docker.</p>
-            <p>Graduando em Análise e Desenvolvimento de Sistemas pelo Instituto Federal do Paraná.</p>
+            <p>Graduado em Análise e Desenvolvimento de Sistemas pelo Instituto Federal do Paraná.</p>
         </div>
     );
 }
